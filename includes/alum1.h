757516de39c66b003dafb7d63d753b6ba4633566
Thomas LE MEUR <tle-meur@e3r2p7.42.fr>
2015-12-20 17:35:45 +0100

    add the first algo and some printing functions

--- includes/alum1.h
+++ includes/alum1.h
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/20 14:14:23 by amulin            #+#    #+#             */
-/*   Updated: 2015/12/20 16:11:45 by amulin           ###   ########.fr       */
+/*   Updated: 2015/12/20 17:10:05 by tle-meur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,14 @@
 
 # include "libft.h"
 
+# define FT_IA			0
+# define FT_USER		1
+# define FT_MIN( x, y )	( ( (x) > (y) ) ? (y) : (x) )
+
 typedef	struct	s_env
 {
 	int	*table;
+	int	*table_ref;
 	int	size;
 	int	id_line;
 }				t_env;
@@ -25,19 +30,19 @@ typedef	struct	s_env
 /*
 ** parsing.c
 */
-int		alcu_readmap_stdin(void);
-int		alcu_readmap_file(char *filename);
-int		alcu_parse_file(char *filename);
+int				alcu_readmap_stdin(void);
+int				alcu_readmap_file(char *filename);
+int				alcu_parse_file(char *filename);
 
 /*
 ** control.c
 */
-int		alcu_run_game(void);
-void	alcu_exit_game(void);
+int				alcu_run_game(void);
+void			alcu_exit_game(void);
 
 /*
 ** error.c
 */
-int		alcu_print_error(char *str);
+int				alcu_print_error(char *str);
 
 #endif
